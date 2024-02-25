6714eb54b9c48f2fcf92afcd097919893334eb20
Pawin Nakvisai <pawinnak2@gmail.com>
2024-02-26 01:14:04 +0700

    [ADD / Frontend] NextAuth & Firebase Authentication

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+import { signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+        redirect('/signin');
+    },
+  });
   return (
-    <main >
-      <div>
-        <h1 className="p-5">PII Data Detector</h1>
-      </div>
-    </main>
+    <div className="p-8">
+      <div className='text-black'>{session?.data?.user?.email}</div>
+      <button className='text-black' onClick={() => signOut()}>Logout</button>
+    </div>
   );
 }
+
+Home.requireAuth = true;
---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import { auth } from "@/app/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+    pages: {
+        signIn: '/signin'
+    },
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {},
+            async authorize(credentials): Promise<any> {
+                return await signInWithEmailAndPassword(auth, (credentials as any).email || '', (credentials as any).password || '')
+                    .then(userCredential => {
+                        if (userCredential.user) {
+                            return userCredential.user;
+                        }
+                        return null;
+                    })
+                    .catch(error => (console.log(error)))
+            }
+        })
+    ],
+}
+
+export default NextAuth(authOptions)
