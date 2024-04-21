0c1211d01b5d529d3f4c030c7807c4afe2b95d38
Pawin Nakvisai <pawinnak2@gmail.com>
2024-04-21 12:31:57 +0700

    [FIX] db.textSetParam does not implement db.TextWithPrismaUserSetParam #2

--- backend/app/queries/text_query.go
+++ backend/app/queries/text_query.go
@@ -4,25 +4,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jedipw/PIIDataDetector/prisma/db"
 	"github.com/jedipw/PIIDataDetector/types"
-	"errors"
 )
 
 func CreateTextWithTitle(c *fiber.Ctx, client *db.PrismaClient, textRequest types.CreateTextWithTitleRequest) (*db.TextModel, error) {
-	user, err := client.User.FindUnique(
-        db.User.UserID.Equals(textRequest.UserID),
-    ).Exec(c.Context())
-
-	if err != nil {
-		// Handle user not found error or other potential errors
-		return nil, err
-	}
-
-	if user == nil {
-		// Handle user not found scenario (e.g., return error)
-		return nil, errors.New("user not found")
-	}
 	createdText, err := client.Text.CreateOne(
-		db.Text.UserID.Set(user.UserID),
+		db.Text.User.Link(
+			db.User.UserID.Equals(textRequest.UserID),
+		),
 		db.Text.TextTitle.Set(textRequest.TextTitle),
 		db.Text.TextContent.Set(""),
 	).Exec(c.Context())
