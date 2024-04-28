e8daca1f893e2efef601e0e9e092ddda977a1000
Pawin Nakvisai <pawinnak2@gmail.com>
2024-04-28 15:18:48 +0700

    [ADD] Login decoration and validation

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,38 +1,89 @@
 'use client';
 import { signIn } from 'next-auth/react';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation';
 
 export default function Signin() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const router = useRouter();
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [errors, setErrors] = useState({
+        email: '',
+        password: '',
+        signInCredential: ''
+    });
+
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const errorParam = searchParams!.get('error'); // Access 'error' parameter using get()
+        if (errorParam) {
+            setErrors({
+                email: '',
+                password: '',
+                signInCredential: 'Invalid email or password. Please try again.'
+            });
+        }
+    }, [searchParams]);
+
+    const validateEmail = (email: string) => {
+        // Return an error message if the email is invalid, otherwise return an empty string
+        if (!email) {
+            return 'Email is required';
+        }
+
+        // Regular expression to validate email address
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return 'Invalid email address';
+        }
+
+        return '';
+    };
+
+    const validatePassword = (password: string) => {
+        // Return an error message if the password is invalid, otherwise return an empty string
+        if (!password) {
+            return 'Password is required';
+        }
+
+        return '';
+    };
+
+    const validateForm = () => {
+        const emailError = validateEmail(email);
+        const passwordError = validatePassword(password);
+
+        setErrors({
+            email: emailError,
+            password: passwordError,
+            signInCredential: ''
+        });
+
+        return !emailError && !passwordError;
+    };
 
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <Image
-                        className="mx-auto"
-                        width="40"
-                        height="40"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
+                <div className="bg-[#F4EEE0] sm:mx-auto sm:w-full sm:max-w-xl pt-20 pb-10 pl-5 pr-5 sm:pl-24 sm:pr-24 rounded-2xl shadow-md">
+                    <h2 className="text-center text-2xl font-extrabold leading-9 tracking-tight text-gray-900">
+                        LOG IN
                     </h2>
-                </div>
+                    <h3 className="text-center text-2xl leading-9 tracking-tight text-gray-900">
+                        Please sign in to continue
+                    </h3>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST" onSubmit={(e) => {
+                    <form className="space-y-6 mt-10" action="#" method="POST" onSubmit={(e) => {
                         e.preventDefault();
+                        setIsSubmitted(true);
                         signIn('credentials', { email, password, redirect: true, callbackUrl: '/' });
                     }}>
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label htmlFor="email" className="block text-sm font-bold leading-6 text-gray-900">
                                 Email address
                             </label>
                             <div className="mt-2">
@@ -43,20 +94,25 @@ export default function Signin() {
                                     autoComplete="email"
                                     onChange={(e) => setEmail(e.target.value)}
                                     required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                {errors.email && <p className="mt-2 text-sm text-red-500">{errors.email}</p>}
                             </div>
                         </div>
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="password" className="block text-sm font-bold leading-6 text-gray-900">
                                     Password
                                 </label>
                                 <div className="text-sm">
-                                    <div onClick={() => router.push('/forgot-password')} className="cursor-pointer font-semibold text-indigo-400 hover:text-indigo-300">
+                                    <button
+                                        type="button"
+                                        onClick={() => router.push('/forgot-password')}
+                                        className="text-sm text-gray-500 font-bold focus:outline-none"
+                                    >
                                         Forget password?
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                             <div className="mt-2">
@@ -67,25 +123,28 @@ export default function Signin() {
                                     autoComplete="current-password"
                                     onChange={(e) => setPassword(e.target.value)}
                                     required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                 />
+                                {errors.password && <p className="mt-2 text-sm text-red-500">{errors.password}</p>}
                             </div>
                         </div>
 
-                        <div>
+                        {errors.signInCredential && <p className="mt-2 text-sm text-red-500">{errors.signInCredential}</p>}
+
+                        <div className="flex justify-center">
                             <button
                                 type="submit"
-                                disabled={!email || !password}
-                                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                disabled={!email || !password || isSubmitted }
+                                className="mt-5 disabled:opacity-40 flex w-48 justify-center rounded-md bg-[#FBBA21] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#E8A300] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#E8A300]"
                             >
                                 Sign in
                             </button>
                         </div>
                     </form>
 
-                    <p className="mt-10 text-center text-sm text-gray-500">
+                    <p className="mt-3 text-center text-sm text-gray-500">
                         Not a member?{' '}
-                        <button onClick={() => router.push('signup')} className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300">
+                        <button onClick={() => router.push('signup')} className="text-sm text-[#E8A300] font-bold ">
                             Sign Up
                         </button>
                     </p>
