7c92457526ab7046256db7b42c75fc1da6e0be3f
Pawin Nakvisai <pawinnak2@gmail.com>
2024-04-28 16:31:23 +0700

    [ADD] Forget password decoration & validation

--- frontend/app/forgot-password/page.tsx
+++ frontend/app/forgot-password/page.tsx
@@ -1,35 +1,49 @@
 'use client';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import Image from 'next/image';
 import { useState } from 'react'
 import { auth } from '../firebase';
 
 export default function ForgotPassword() {
 
     const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+
+    const validateEmail = () => {
+        // Return an error message if the email is invalid, otherwise return an empty string
+        if (!email) {
+            setError('Email is required');
+            return false;
+        }
+
+        // Regular expression to validate email address
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setError('Invalid email address');
+            return false;
+        }
+
+        return true;
+    };
 
     const resetEmail = () => {
-        sendPasswordResetEmail(auth, email);
+        if (validateEmail()) {
+            sendPasswordResetEmail(auth, email);
+        }
     }
+
 
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <Image
-                        className="mx-auto"
-                        width="40"
-                        height="40"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Forgot Password
+                <div className="bg-[#F4EEE0] sm:mx-auto sm:w-full sm:max-w-xl pt-20 pb-10 pl-5 pr-5 sm:pl-24 sm:pr-24 rounded-2xl shadow-md">
+                    <h2 className="text-center text-2xl font-extrabold leading-9 tracking-tight text-gray-900">
+                        FORGET PASSWORD
                     </h2>
-                </div>
+                    <h3 className="text-center text-2xl leading-9 tracking-tight text-gray-900">
+                        Enter the email address associate with your account.
+                    </h3>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST" onSubmit={(e) => {
+                    <form className="space-y-6 mt-10" action="#" method="POST" onSubmit={(e) => {
                         e.preventDefault();
                         resetEmail();
                     }}>
@@ -45,16 +59,17 @@ export default function ForgotPassword() {
                                     autoComplete="email"
                                     onChange={(e) => setEmail(e.target.value)}
                                     required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                 />
+                                {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
                             </div>
                         </div>
 
-                        <div>
+                        <div className="flex justify-center">
                             <button
                                 type="submit"
                                 disabled={!email}
-                                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="mt-5 disabled:opacity-40 flex w-64 justify-center rounded-md bg-[#FBBA21] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#E8A300] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#E8A300]"
                             >
                                 Send Forgot Password Email
                             </button>
