8d77eeb43fb57acba24f4be9b6aac9f18da9a2d1
Pawin Nakvisai <pawinnak2@gmail.com>
2024-05-10 21:19:56 +0700

    [ADD] PII Endpoint

--- pii_data_detector/main.py
+++ pii_data_detector/main.py
@@ -1,8 +1,18 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+pii_get_args = reqparse.RequestParser()
+pii_get_args.add_argument("text", type=str, help="Text that will be used to detect PII")
+
+class PII(Resource):
+    def get(self):
+        args = pii_get_args.parse_args()
+        return args
+    
+api.add_resource(PII, "/pii")
 
 if __name__ == "__main__":
     app.run(debug=True)
